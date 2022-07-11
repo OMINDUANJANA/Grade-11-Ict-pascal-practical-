3158bff5ada939c813523eda18849cd3a8cc6c70
Omindu Anjana <93399269+OMINDUANJANA@users.noreply.github.com>
2022-07-11 18:32:53 +0530

    Create circleArea.pas

---
+++ circleArea.pas
@@ -0,0 +1,9 @@
+program circle (input,output);
+var radious : integer;
+var area
+begin
+  write("Enter Radius");
+  read (radius);
+  area:= 22/7 * radius * radius;
+  writeln ("Area Is",area);
+end.
