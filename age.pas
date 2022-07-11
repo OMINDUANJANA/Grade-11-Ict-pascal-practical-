7a28c5fc0aa28972a22ad37cf39f861f02cbb3ab
Omindu Anjana <93399269+OMINDUANJANA@users.noreply.github.com>
2022-07-11 18:12:06 +0530

    Create age.pas

---
+++ age.pas
@@ -0,0 +1,11 @@
+
+program age (input,output);
+var age:integer,
+begin
+ writeln ("Enter Your Age");
+ readln (age);
+ if age >=18 Then 
+ writeln ("You Are And Adult");
+ Else
+ writeln ("You Are And Child");
+end.
